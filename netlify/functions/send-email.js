7ac8432708604cffbd0ceb7194ab0b583a2cf94e
TercanoNuruva <thyoung89@gmail.com>
2026-09-23 09:45:12 -0600

    forgot a file

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -97,7 +97,7 @@ exports.handler = async (event, context) => {
     // Send email using Resend (same as your original code)
     const data = await resend.emails.send({
       from: "requests@guidedpathcenter.com",
-      to: ["bethany@guidedpathcenter.com"],
+      to: ["bethany@guidedpathcent er.com"],
       // to: ["thyoung89@gmail.com"],
       subject: "New Waitlist Form Submission",
       html: htmlContent,
